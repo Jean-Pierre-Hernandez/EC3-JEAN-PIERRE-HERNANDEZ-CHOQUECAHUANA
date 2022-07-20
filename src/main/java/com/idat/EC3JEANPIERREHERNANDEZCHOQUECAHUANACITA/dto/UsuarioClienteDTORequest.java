d2ec43879eb5e59555d024cc1c591e22f4d51c65
Jean Pierre Hernandez <63524899+Jean-Pierre-Hernandez@users.noreply.github.com>
2022-07-20 00:36:05 -0500

    Add files via upload

---
+++ src/main/java/com/idat/EC3JEANPIERREHERNANDEZCHOQUECAHUANACITA/dto/UsuarioClienteDTORequest.java
@@ -0,0 +1,35 @@
+package com.idat.EC3JEANPIERREHERNANDEZCHOQUECAHUANACITA.dto;
+
+public class UsuarioClienteDTORequest {
+	private Integer idUsuarioCliente;
+	private String usuario;
+	private String password;
+
+	
+	public Integer getIdUsuarioCliente() {
+		return idUsuarioCliente;
+	}
+
+	public void setIdUsuarioCliente(Integer idUsuarioCliente) {
+		this.idUsuarioCliente = idUsuarioCliente;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
